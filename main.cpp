0332102032f64808f6fc0d97f49bd2d2b2bf1237
ayoub Er-razki <ayouberrazki@outlook.com>
2020-09-19 22:58:39 +0200

    fixing update rooms

--- main.cpp
+++ main.cpp
@@ -23,7 +23,16 @@ ListAllRooms() {
   }
 }
 
-void updateRoomByName(string name, Room & m) { //new object
+bool searchForRoom(string name) {
+  vector < Room > ::iterator itr;
+  bool found = false;
+  for (itr = rooms.begin(); itr != rooms.end(); ++itr) {
+    if (name.compare(itr -> getName()) == 0) return found = true;
+  }
+  return found;
+}
+
+bool updateRoomByName(string name, Room & m) { //new object
   vector < Room > ::iterator itr;
   bool found = false;
   for (itr = rooms.begin(); itr != rooms.end(); ++itr) {
@@ -31,8 +40,10 @@ void updateRoomByName(string name, Room & m) { //new object
     if (name.compare(itr -> getName()) == 0) {
       found = true;
       Room & toupdate = * itr;
-      cout << "the M :" << m.getName() << endl;
+      //cout << "the M :" << m.getName() << endl;
 
+      * itr = m;
+      /*
       toupdate.setName(m.getName());
       toupdate.setWidth(m.getWidth());
       toupdate.setColor(m.getColor());
@@ -41,9 +52,10 @@ void updateRoomByName(string name, Room & m) { //new object
       cout << toupdate;
       cout << "--listing all rooms--" << endl;
       ListAllRooms();
+      */
     }
   }
-  if (found == false) cout << "Room Not found !" << endl;
+  return found;
 
 }
 
@@ -84,7 +96,61 @@ void ListRoomByName(string name) {
 
 void Quit() {}
 
+void addRoomDisplay() {
+  int id;
+  float length, width = 0;
+  string color, name;
+
+  cout << "[+]Enter the id[+]" << endl;
+  cin >> id;
+  cout << "[+]Enter the name[+]" << endl;
+  cin >> name;
+  cout << "[+]Enter length[+]" << endl;
+  cin >> length;
+  cout << "[+]Enter width[+]" << endl;
+  cin >> width;
+  cout << "[+]Enter Color[+]" << endl;
+  cin >> color;
+  Room r(id, name, length, width, color);
+
+  addRoom(r);
+}
+
+void updateHotelRoomDisplay() {
+  string room_to_find;
+  int newid;
+  float newlength, newwidth;
+  string newname, newcolor;
+
+  cout << "[!] Enter the Room name to search [!]" << endl;
+  cin >> room_to_find;
+
+  if (!searchForRoom(room_to_find)) {
+    cout << "Room not found !" << endl;
+
+  } else {
+    cout << "ROOM FOUND: " << room_to_find << endl;
+    cout << "[+]Enter the New id for the room[+]" << endl;
+    cin >> newid;
+    cout << "[+]Enter the New name for the room[+]" << endl;
+    cin >> newname;
+    cout << "[+]Enter the New length for the room[+]" << endl;
+    cin >> newlength;
+    cout << "[+]Enter the New width for the room[+]" << endl;
+    cin >> newwidth;
+    cout << "[+]Enter the New Color for the room[+]" << endl;
+    cin >> newcolor;
+
+    Room r(newid, newname, newlength, newwidth, newcolor);
+    updateRoomByName(room_to_find, r);
+
+    ListAllRooms();
+  }
+
+}
+
 int main() {
+
   Room room1(1, "Valencia", 23.1, 23.1, "Blue");
   Room room2(2, "Royal Flamingo", 70.5, 50.3, "Purple");
   Room room3(3, "Esperanda", 40.1, 30.1, "Pink");
@@ -99,9 +165,9 @@ int main() {
   //searchByLengthWidth(40.1,30.1);
   //ListRoomWithMaxArea();
   //ListRoomByName("Valencia");
-  ListAllRooms();
-  Room toupdate(66, "toupdateName", 12, 14, "Blue");
-  updateRoomByName("Royal Flamingo", toupdate);
+
+  // addRoomDisplay();
+  updateHotelRoomDisplay();
 
   return 0;
 }
